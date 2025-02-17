d6b6b8efca75806a86e4511a31f7398b65caa490
Joem Bolinas <lr.jbolinas@mmdc.mcl.edu.ph>
2025-02-17 14:02:21 +0800

    Add Employee class to store employee data

---
+++ src/com/motorph/payroll/Employee.java
@@ -0,0 +1,24 @@
+
+
+// Employee.java - Stores Employee Data
+public class Employee {
+    public int employeeNumber = 10001; // Employee Number
+    public String lastName = "Garcia";
+    public String firstName = "Manuel III";
+    public String birthday = "10/11/1983";
+    public String address = "Valero Carpark Building, Valero Street 1227, Makati City";
+    public String phoneNumber = "966-860-270";
+    public String sssNumber = "44-4506057-3";
+    public String philhealthNumber = "820126853951";
+    public String tinNumber = "442-605-657-000";
+    public String pagibigNumber = "691295330870";
+    public String status = "Regular";
+    public String position = "Chief Executive Officer";
+    public String immediateSupervisor = "N/A";
+    public double basicSalary = 90000;
+    public double riceSubsidy = 1500;
+    public double phoneAllowance = 2000;
+    public double clothingAllowance = 1000;
+    public double grossSemiMonthlyRate = 45000;
+    public double hourlyRate = 535.71;
+}
