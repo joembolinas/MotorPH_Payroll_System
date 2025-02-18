eec0333592eaef234e212e0539bd0f8dbcf462dc
Joem Bolinas <lr.jbolinas@mmdc.mcl.edu.ph>
2025-02-19 04:49:21 +0800

    Employee Data Display

--- src/com/motorph/payroll/AttendanceTracker.java
+++
@@ -1,7 +0,0 @@
-// AttendanceTracker.java - Tracks Employee Work Hours
-public class AttendanceTracker {
-    // Returns the total hours worked
-    public static double getHoursWorked(double hoursWorked) {
-        return hoursWorked;
-    }
-}
--- src/com/motorph/payroll/Main.java
+++ src/com/motorph/payroll/Main.java
@@ -1,5 +1,6 @@
 package com.motorph.payroll;
 
+
 import java.io.IOException;
 import java.util.List;
 
